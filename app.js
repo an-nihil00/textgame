101e93b4fa38eaa31aa6c17e2894bb16b5277715
an-nihil00 <32604310+an-nihil00@users.noreply.github.com>
2021-11-30 13:00:07 -0600

    form validation a little bit

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+function scrollToBottom() {
+    window.scrollTo(0, document.body.scrollHeight)
+}
+
 function endText(s,beginning) {
     return () => {
 	var p = document.createElement("p");
@@ -5,13 +9,13 @@ function endText(s,beginning) {
 	var link = document.createElement("a");
 	link.href = "#";
 	link.onclick = function () {
-	    document.body.textContent = ""
-	    setTimeout(beginning(),100);
+	    location.reload();
 	}
 	link.innerText = "Restart?"
 	p.appendChild(document.createElement("br"));
 	p.appendChild(link)
 	document.body.appendChild(p)
+	scrollToBottom()
     };
 }
 
@@ -24,14 +28,16 @@ function printText(s, next) {
 	p.appendChild(document.createElement("br"));
 	p.appendChild(prompt);
 	document.body.appendChild(p)
-	setTimeout(() => {document.addEventListener("keyup", ({key}) => {
+	var eventHandler = ({key}) => {
 	    if (key === "Enter") {
 		p.removeChild(prompt);
 		p.className = "read";
-		document.removeEventListener("keyup", this);
+		document.removeEventListener("keyup", eventHandler);
 		setTimeout(next(),10);
 	    }
-	})},1);
+	}
+	setTimeout(() => {document.addEventListener("keyup", eventHandler)},1);
+	scrollToBottom()
     };
 }
 
@@ -68,25 +74,29 @@ function multipleChoiceBranch(prompt, branches) {
 	var p = document.createElement("p");
 	p.innerText = prompt;
 	var c = document.createElement("ul");
-	for (const branch in branches) {
-	    var li = document.createElement("li");
-	    var link = document.createElement("a");
-	    link.href = "#";
-	    link.onclick = function () {
-		var e = document.createElement("i");
-		e.innerText = branch;
-		p.removeChild(c);
-		p.appendChild(document.createElement("br"));
-		p.appendChild(e);
-		p.className = "read";
-		setTimeout(branches[branch](),10);
+	branches.forEach(function (val, i) {
+	    if (!val[2]) {
+		var li = document.createElement("li");
+		var link = document.createElement("a");
+		link.href = "#";
+		link.onclick = function () {
+		    var e = document.createElement("i");
+		    e.innerText = val[0];
+		    p.removeChild(c);
+		    p.appendChild(document.createElement("br"));
+		    p.appendChild(e);
+		    p.className = "read";
+		    val[2] = true;
+		    setTimeout(val[1](),10);
+		}
+		link.innerText = val[0];
+		li.appendChild(link);
+		c.appendChild(li)
 	    }
-	    link.innerText = branch;
-	    li.appendChild(link);
-	    c.appendChild(li);
-	}
+	})
 	p.appendChild(c);
 	document.body.appendChild(p);
+	scrollToBottom()
     }
 }
 
@@ -98,13 +108,27 @@ function textEntry(prompt, target, next) {
 	i.type = "text"
 	i.addEventListener("keyup", ({key}) => {
 	    if (key === "Enter") {
-		window[target] = i.value;
-		var e = document.createElement("i");
-		e.innerText = i.value;
-		p.removeChild(i);
-		p.appendChild(e);
-		p.className = "read";
-		setTimeout(next(),10);
+		if (i.value) {
+		    window[target] = i.value;
+		    var e = document.createElement("i");
+		    e.innerText = i.value;
+		    p.removeChild(i);
+		    p.appendChild(e);
+		    p.className = "read";
+		    setTimeout(next(),10);
+		    return;
+		}
+		if (document.body.lastChild.tagName != "I") {
+		    var e = document.createElement("i");
+		    e.innerText = "Enter some text"
+		    document.body.appendChild(e);
+		    setTimeout(() => {
+			e.style.opacity = '0';
+			setTimeout(() => {
+			    document.body.removeChild(e)
+			}, 500)
+		    }, 1000)
+		}
 	    }
 	})
 	p.appendChild(document.createElement("br"));
@@ -138,10 +162,10 @@ var part3 = branch("char_class",{
 
 var wanderer = printText(() => "You have journeyed long and far but finally you have come to the imperial city, on the eve of the winter solstice. The sight of the great palace looms over the firelit streets of the city but all you can think of is a hot meal and a refuge from the biting cold.", () => wanderer2);
 
-var wanderer2 = multipleChoiceBranch("Where shall you go?",{
-    "The bazaar":end,
-    "The temple":() => temple1
-})
+var wanderer2 = multipleChoiceBranch("Where shall you go?",[
+    ["The bazaar", end],
+    ["The temple", () => temple1]
+])
 
 var priest = printText(() => "On this, your last winter solstice as a neophyte in the great temple of the sun, you find yourself in the familiar position of preparing food for the communal meal of the temple. And yet this year you can feel your hands shake and your stomach turn itself in knots with the awareness that you will never see this occasion as a youth again.", end)
 
@@ -151,6 +175,20 @@ var elf = printText(() => "Your restless journey has brought you to the capital 
 
 var temple1 = printText(() => "You walk west along the imperial road and through the gate of the old city. Turning north you head toward the sacred mount, upon which stands the temple of the Sun. The street is full of people: pilgrims and citizens, nobles and commoners. The whole crowd's movements become synchronized and you simply let yourself be swept along, climbing the great hill in a dreamlike haze.", () => temple2)
 
-var temple2 = printText(() => "Unable to tell if an eternity has passed or if it has only been an instant, you find yourself at the summit, before the open doors of the temple.", end)
+var temple2 = multipleChoiceBranch("Unable to tell if an eternity has passed or if it has only been an instant, you find yourself at the summit, before the open doors of the eastern entrance of the temple.", [
+    ["Examine the temple",() => templeDesc],
+    ["Enter",() => temple3]
+])
+
+var templeDesc = multipleChoiceBranch("You face the doors of a vestibule on the eastern wall of the temple, which leads directly into the temple's grand sanctuary, whose circular dome looms above you. To the north you can see the central nave, ending in the great gate which is used only for official processions. This connects to the road of sacrifice, which proceeds down the sacred mount, through the northern part of the city and into the wilderness beyond. To the south you can see a large garden; it is empty now but you suspect it would be quite a pleasant place in the spring or summer. The whole building is intricately ornamented, covered in sculpture and relief carvings, their beauty recalling the stories of the divine cities which fell to ruin so long ago.", [
+    ["Enter", () => temple3]
+])
+
+var temple3 = printText(() => "Stepping into the temple you feel suddenly warm, some magic of the priests no doubt. Looking around, you seem young acolytes swarming around the edges of the sanctuary, carrying food and drink to great serving vessels which the visitors to the temple appear to take from as they please. In the center, many people sit on the floor and eat. You have heard that the priests of this land have a custom of serving communal meals in their temples to any who will come, and as you look out you see people of all social classes seated together on the floor, distinguishable by their clothing and yet not separated. ", () => temple4)
+
+var temple4 = multipleChoiceBranch("Your stomach reminds you that you have been traveling a week now with nothing to eat but tallow, dried meat, and juniper berries. The enticing aromas of spiced lamb, rice and fragrant tea call to you. And yet, maybe it would be better to leave; priests do always seem to have some disguised motive behind their generosity.", [
+    ["Grab some food", end],
+    ["Leave",() => wanderer2]
+])
 
 part1();
