07850acbd92d5319e59a4873118fdebe851435d6
an-nihil00 <32604310+an-nihil00@users.noreply.github.com>
2021-11-30 10:26:58 -0600

    added choice branches

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ function printText(s, next) {
     };
 }
 
-function multipleChoice(prompt, choices, target, next) {
+function multipleChoiceVar(prompt, choices, target, next) {
     return () => {
 	var p = document.createElement("p");
 	p.innerText = prompt;
@@ -46,6 +46,33 @@ function multipleChoice(prompt, choices, target, next) {
 		next()();
 	    }
 	    link.innerText = choice;
+	    li.appendChild(link);
+	    c.appendChild(li);
+	}
+	p.appendChild(c);
+	document.body.appendChild(p);
+    }
+}
+
+function multipleChoiceBranch(prompt, branches) {
+    return () => {
+	var p = document.createElement("p");
+	p.innerText = prompt;
+	var c = document.createElement("ul");
+	for (const branch in branches) {
+	    var li = document.createElement("li");
+	    var link = document.createElement("a");
+	    link.href = "#";
+	    link.onclick = function () {
+		var e = document.createElement("i");
+		e.innerText = branch;
+		p.removeChild(c);
+		p.appendChild(document.createElement("br"));
+		p.appendChild(e);
+		p.className = "read";
+		branches[branch]()();
+	    }
+	    link.innerText = branch;
 	    li.appendChild(link);
 	    c.appendChild(li);
 	}
@@ -91,7 +118,7 @@ function random_branch(branches) {
 }
 var end = () => endText("there is nothing more for now");
 
-var part1 = multipleChoice("Who are you?", ["wanderer","thief","priest","elf"], "char_class", () => part2);
+var part1 = multipleChoiceVar("Who are you?", ["wanderer","thief","priest","elf"], "char_class", () => part2);
 var part2 = textEntry(() => `What is your name, ${char_class}?`,"char_name", () => part3);
 var part3 = branch("char_class",{
     "elf":() => elf,
@@ -100,7 +127,12 @@ var part3 = branch("char_class",{
     "priest":() => priest,
 })
 
-var wanderer = printText(() => "You have journeyed long and far but finally you have come to the imperial city, on the eve of the winter solstice. The sight of the great palace looms over the firelit streets of the city but all you can think of is a hot meal and a refuge from the biting cold.", end);
+var wanderer = printText(() => "You have journeyed long and far but finally you have come to the imperial city, on the eve of the winter solstice. The sight of the great palace looms over the firelit streets of the city but all you can think of is a hot meal and a refuge from the biting cold.", () => wanderer2);
+
+var wanderer2 = multipleChoiceBranch("Where shall you go?",{
+    "The Bazaar":end,
+    "The Temple":end
+})
 
 var priest = printText(() => "On this, your last winter solstice as a neophyte in the great temple of the sun, you find yourself in the familiar position of preparing food for the communal meal of the temple. And yet this year you can feel your hands shake and your stomach turn itself in knots with the awareness that you will never see this occasion as a youth again.", end)
 
